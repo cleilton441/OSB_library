214c6ecb14c3d2d6cb540c96656b4266ab8e9892
Cleilton Brigido <cleilton.brigido@gmail.com>
2021-07-04 21:55:23 -0300

    segundo commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Input from './componets/input/Input';
 import Button from './componets/button/Button';
 import Requests from './componets/requests/Requests';
 import api from './services/api';
-
+import './App.css'
 
 function App() {
 
@@ -23,16 +23,22 @@ function App() {
   
   }
 
-  return (
-    <>
+  return ( 
+    <> 
+    <div className="App"> 
       <Title/>
+      <div className="main">
       <Input setText={setText}/>
       <Button onclick={getData}/>
+      </div>
+      <div className="requests">
       {data.length ? 
               data.map((response:any, key: number)=> (
       <Requests  author={response.author ? response.author : "Autor não informado"} title={response.title ? response.title : "Titulo não informado"} url={response.url ? response.url : "Url não informada"} key={key} />
               )) : <p/>}
-      </> 
+      </div>
+      </div> 
+      </>
   );
 }
 
